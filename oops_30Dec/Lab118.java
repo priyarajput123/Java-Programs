d63e9adfb65d958e582c5ace85239935c3a9ed70
priyarajput123 <priya9156rajput@gmail.com>
2024-01-09 21:22:13 +0530

    Java Program

---
+++ oops_30Dec/Lab118.java
@@ -0,0 +1,9 @@
+package src.oops_30Dec;
+
+public class Lab118 {
+    public static void main(String[] args) {
+        System.out.println("This is a next with new Line");
+        int age =10;
+        System.out.printf("%d is you age", age);
+    }
+}
